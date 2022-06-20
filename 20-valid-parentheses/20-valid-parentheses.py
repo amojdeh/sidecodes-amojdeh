60f653d22a55fdd46dfd6bcd9d723ae3c8f82a5c
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-06-20 15:37:24 -0400

    Time: 82 ms (5.24%), Space: 14 MB (23.77%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -4,4 +4,4 @@ class Solution:
         for i in range(len(s)):
             if len(s) != 0:
                 s = s.replace('()','').replace('{}','').replace('[]','')
-        return len(s) == 0 
+        return s == ''
