7b49454d78f93cb2904e178cba447ec9868834e7
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-06-21 12:57:52 -0400

    Time: 83 ms (25.41%), Space: 13.8 MB (56.33%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,12 +1,32 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        i = 0
         
-        while True:
+        ######## my code ##########
+#         i = 0
+        
+#         while True:
             
-            if i*i == x:
-                return i
-            elif i*i < x < (i+1)*(i+1):
-                return i
+#             if i*i == x:
+#                 return i
+#             elif i*i < x < (i+1)*(i+1):
+#                 return i
             
-            i += 1
+#             i += 1
+
+######### optimal solution #########
+
+        if x == 0:
+            return 0
+        left = 1
+        right = x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid > x:
+                right = mid
+            elif mid * mid < x:
+                if mid == left:
+                    return mid
+                left = mid
+            else:
+                return mid
+        return None
