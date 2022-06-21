47a37e3aad5fbdc67ea31f8dc269cc2a58f04e5c
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-06-21 12:09:14 -0400

    Time: 60 ms (20.06%), Space: 13.9 MB (58.01%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,11 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        # if digits[-1] < 9:
-        #     d = digits[-1]
-        #     digits.pop()
-        #     digits.append(d + 1)
-        #     return digits
-        
+    
         s = ""
         for item in digits:
             s += str(item)
