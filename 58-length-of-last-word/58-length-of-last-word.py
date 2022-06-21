0cc88bcf7bb762f2d3ef87fc89ff36ceb5f68e21
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-06-21 11:52:25 -0400

    Time: 65 ms (6.87%), Space: 13.9 MB (37.66%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.py
+++ 58-length-of-last-word/58-length-of-last-word.py
@@ -1,3 +1,5 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         return len(s.split()[-1])
+    
+    ############ s.strip() can be used to remove spaces in the beginning and end of the string############
