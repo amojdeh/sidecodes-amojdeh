d76c5b5ce88cb0ddc8c0aa05fbf2f4d3ea4ecfe3
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-06-27 18:32:18 -0400

    Time: 70 ms (13.69%), Space: 13.8 MB (68.16%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.py
@@ -2,18 +2,14 @@ import string as st
 class Solution:
     def getLucky(self, s: str, k: int) -> int:
         
-        
-        
         new_s = ''
         for i in s:
             new_s += str(st.ascii_lowercase.index(i) + 1)
         print(new_s)
         
         while k > 0:
-            count = 0
-            for i in new_s:
-                count += int(i)
-                
+            
+            count = sum(int(i) for i in new_s)    
             k -= 1
             new_s = str(count)
         return count
