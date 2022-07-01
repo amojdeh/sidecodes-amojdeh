aae2726da52d76a99d9edaf69b41b836e7f7bc02
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-07-01 17:05:13 -0400

    Time: 819 ms (5.01%), Space: 14 MB (61.43%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -1,3 +1,15 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        return str(int(num1) + int(num2))
+        
+        first = 0
+        second = 0
+        
+        for i in range(len(num1)):
+            
+            first += (int(num1[i])*10**(len(num1)-(i+1)))
+            
+        for i in range(len(num2)):
+            
+            second += (int(num2[i])*10**(len(num2)-(i+1)))
+            
+        return str(first + second)
