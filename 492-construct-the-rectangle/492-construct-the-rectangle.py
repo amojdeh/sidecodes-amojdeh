f11784236e0cf91f4e9ef650f2700e3fa258900f
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-07-03 15:40:44 -0400

    Time: 74 ms (27.91%), Space: 13.9 MB (57.70%) - LeetHub

--- 492-construct-the-rectangle/492-construct-the-rectangle.py
+++ 492-construct-the-rectangle/492-construct-the-rectangle.py
@@ -1,24 +1,34 @@
 class Solution:
     def constructRectangle(self, area: int) -> List[int]:
         
-        w = int(sqrt(area))
-        size = {}
-        diff = {}
-        i = 0
-        while w > 0:
-            l = area / w
-            if int(l) == l:
-                size[i] = [int(l),int(w)]
+        #### first solution
+#         w = int(sqrt(area))
+#         size = {}
+#         diff = {}
+#         i = 0
+#         while w > 0:
+#             l = area / w
+#             if int(l) == l:
+#                 size[i] = [int(l),int(w)]
                 
-                diff[i] = l - w
+#                 diff[i] = l - w
                 
-                i += 1
-            w -= 1
-        try:    
-            min_ind = list(diff.values()).index(min(diff.values()))
-        except:
-            return [area,1]
+#                 i += 1
+#             w -= 1
+#         try:    
+#             min_ind = list(diff.values()).index(min(diff.values()))
+#         except:
+#             return [area,1]
         
-        k = list(diff.keys())[min_ind]
-        return size[k]
+#         k = list(diff.keys())[min_ind]
+#         return size[k]
             
+        
+        #### second solution
+        
+            w = int(sqrt(area))
+            
+            while area % w:
+                
+                w -= 1
+            return [int(area/w), w]
