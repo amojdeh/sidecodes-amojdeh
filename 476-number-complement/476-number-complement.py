bc8ab8b9d1a87e091e393bd421d881c61e51e15c
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-07-03 13:51:16 -0400

    Time: 52 ms (31.43%), Space: 13.9 MB (54.66%) - LeetHub

--- 476-number-complement/476-number-complement.py
+++ 476-number-complement/476-number-complement.py
@@ -1,11 +1,15 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        new = ''
-        for i in bin(num)[2:]:
+#         new = ''
+#         for i in bin(num)[2:]:
             
-            if i == '1':
-                new += '0'
-            elif i == '0':
-                new += '1'
+#             if i == '1':
+#                 new += '0'
+#             elif i == '0':
+#                 new += '1'
     
-        return int(new, 2)
+#         return int(new, 2)
+    
+    
+        new = ''.join('1' if x == '0' else '0' for x in bin(num)[2:])
+        return int(new,2)
