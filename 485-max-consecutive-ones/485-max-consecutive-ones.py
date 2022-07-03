46c572a6580ac400b8a42a85a019ad6bf860fde6
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-07-03 15:01:03 -0400

    Time: 373 ms (86.83%), Space: 14.3 MB (79.07%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.py
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -5,17 +5,19 @@ class Solution:
         
         if 1 not in set(nums):
             return 0
-        for i in range(len(nums)):
-            
-            if nums[i] == 1:
-                if c in count:
-                    count[c] += 1
-                    
-                else:
-                    count[c] = 1
-            
-            elif nums[i] == 0:
-                c += 1
+        
+        else:
+            for i in range(len(nums)):
+
+                if nums[i] == 1:
+                    if c in count:
+                        count[c] += 1
+
+                    else:
+                        count[c] = 1
+
+                elif nums[i] == 0:
+                    c += 1
                     
                     
         return max(list(count.values()))
