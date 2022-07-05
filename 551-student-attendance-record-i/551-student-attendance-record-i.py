a0abdaccdb6ba2cc409578f23d4601fe9ac7d10e
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-07-05 18:21:48 -0400

    Time: 43 ms (59.28%), Space: 13.8 MB (56.37%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.py
+++ 551-student-attendance-record-i/551-student-attendance-record-i.py
@@ -5,23 +5,9 @@ class Solution:
             if s != 'AA':
                 return True
             
-            
-        
         if s.count('A') < 2:
-            
-            for i in range(len(s)):
-                
-                if s[i] == 'L':
-                    if i != 0 and i != len(s) - 1:
-                        if s[i-1] == 'L' and s[i+1] == 'L':
-                            return False
-                    elif i == 0:
-                        if s[2] == 'L' and s[1] == 'L':
-                            return False
-                    elif i == len(s) - 1:
-                        if s[-2] == 'L' and s[-3] == 'L':
-                            return False
-            return True
+            if 'LLL' not in s:
+                return True
         return False
                         
                         
