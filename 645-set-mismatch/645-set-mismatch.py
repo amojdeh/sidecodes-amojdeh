d2644be3e313134abc98e60ec238c663bbae9b97
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-07-08 18:05:13 -0400

    Time: 193 ms (97.46%), Space: 15.8 MB (35.14%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.py
+++ 645-set-mismatch/645-set-mismatch.py
@@ -2,7 +2,6 @@ class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         d = {}
         l = list(range(1, len(nums)+1))
-        print(l)
         ans = []
         for i in nums:
             if i in d:
