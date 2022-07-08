c5faa4fb2d9c33dbf636031ab60d5f4646db001d
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-07-08 17:30:04 -0400

    Time: 543 ms (9.58%), Space: 33.5 MB (21.68%) - LeetHub

--- 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.py
@@ -11,22 +11,6 @@ class Solution:
         
         else:
             nums.sort(reverse = True)
-            
-            if len(nums) < 6:
-                
-                if all(x>0 for x in nums):
-                    return nums[0]*nums[1]*nums[2]
-                elif all(x<0 for x in nums):
-                    return nums[0]*nums[1]*nums[2]
-                else:
-                    return max(nums[0]*nums[1]*nums[2],nums[0]*nums[-1]*nums[-2])
-                        
-                    
-                    
-            else:
-                new = [nums[0], nums[1], nums[2], nums[-3], nums[-2], nums[-1]]
-                return max(new[0]*new[1]*new[2],new[0]*new[-1]*new[-2])
-                
-            
+            return max(nums[0]*nums[1]*nums[2], nums[0]*nums[-1]*nums[-2])
                 
                 
