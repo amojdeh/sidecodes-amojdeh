df8e3506bfa8197d77bb4bfe6b1d6ab40adc1c5b
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-07-22 16:16:50 -0400

    Time: 37 ms (92.57%), Space: 14 MB (37.14%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.py
@@ -0,0 +1,15 @@
+import operator
+from collections import Counter
+class Solution:
+    def mostCommonWord(self, s: str, banned: List[str]) -> str:
+        
+        s = s.replace("!", ' ').replace("?", ' ').replace("'", ' ').replace(",", ' ').replace(";", ' ').replace(".", ' ').lower()
+        
+        d = dict(Counter(s.split()))
+        
+        k = max(d.items(), key=operator.itemgetter(1))[0]
+        while k in banned:
+            d.pop(k)
+            k = max(d.items(), key=operator.itemgetter(1))[0]
+        return k
+            
