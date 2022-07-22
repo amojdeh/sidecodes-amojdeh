0d9eff6cbc0cf07b466090573f71d8506a7b39be
amojdeh <90632660+amojdeh@users.noreply.github.com>
2022-07-22 16:17:41 -0400

    Time: 44 ms (79.75%), Space: 14 MB (37.14%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -6,7 +6,7 @@ class Solution:
         s = s.replace("!", ' ').replace("?", ' ').replace("'", ' ').replace(",", ' ').replace(";", ' ').replace(".", ' ').lower()
         
         d = dict(Counter(s.split()))
-        
+        banned = set(banned)
         k = max(d.items(), key=operator.itemgetter(1))[0]
         while k in banned:
             d.pop(k)
